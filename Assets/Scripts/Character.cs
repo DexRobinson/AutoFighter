cbc7f780119dfc2ca5edf4a812ef1956bd8195b0
Dex Robinson <drobinson@connectwise.com>
2020-09-23 23:32:43 -0400

    test
    trest

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private string chatacterName;
+
+    private float currentHealth;
+    private float maxHealth;
+
+    private float attackDamage;
+    private float armor;
+    private float attackSpeed;
+    private float attackRange;
+    private float movementSpeed;
+
+    public virtual void Death()
+    {
+        
+    }
+
+    public void RemoveHealth(float amt)
+    {
+        float newHealth = currentHealth - (amt - armor);
+        currentHealth = Mathf.Clamp(newHealth, 1, amt);
+        if(currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+}
